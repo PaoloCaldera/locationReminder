6477bdd3f28f324473b16ee3f1788cb731cc224d
PaoloCaldera <paolo.caldera@outlook.com>
2023-07-23 21:31:47 +0200

    Change activity launching as in the link

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -38,22 +40,16 @@ class AuthenticationActivity : AppCompatActivity() {
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == LOGIN_REQUEST_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                Log.i(TAG,
-                    "Successful login of ${FirebaseAuth.getInstance().currentUser?.displayName}"
-                )
-                // Go back into the RemindersActivity cleaning the back stack
-                exitLogin()
-            } else {
-                Log.i(TAG, "Unsuccessful login: ${response?.error?.errorCode}")
-            }
+    private val loginLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            Log.i(TAG,"Successful login of ${FirebaseAuth.getInstance().currentUser?.displayName}")
+            // Go back into the RemindersActivity cleaning the back stack
+            exitLogin()
+        } else {
+            Log.i(TAG, "Unsuccessful login: ${it.idpResponse?.error?.errorCode}")
         }
     }
+
 
     private fun launchSignInFlow() {
 
@@ -68,14 +64,13 @@ class AuthenticationActivity : AppCompatActivity() {
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setTheme(R.style.AppTheme)
-                .build(),
-            LOGIN_REQUEST_CODE
-        )
+        val loginIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.AppTheme)
+            .build()
+
+        loginLauncher.launch(loginIntent)
     }
 
     private fun exitLogin() {
