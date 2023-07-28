d45ee54d3623a769436f447e5dd2df0c271e11e9
PaoloCaldera <paolo.caldera@outlook.com>
2023-07-28 12:32:40 +0200

    Update UI tests for ReminderListFragment

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,16 +1,14 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
-import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -21,7 +19,7 @@ import com.udacity.project4.locationreminders.data.FakeAndroidDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.utils.ServiceLocator
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
@@ -56,7 +54,7 @@ class ReminderListFragmentTest {
     }
 
     @Test
-    fun reminderListFragment_reminderList_checkContent() = runTest {
+    fun reminderListFragment_reminderList_checkContent() = runBlockingTest {
         // GIVEN: instantiate a reminder and save it into the data source
         val reminder = ReminderDTO(
             title = "Apples",
@@ -71,7 +69,6 @@ class ReminderListFragmentTest {
         launchFragmentInContainer<ReminderListFragment>(themeResId = R.style.AppTheme)
 
         // THEN: check that data is displayed
-
         onView(withId(R.id.reminderssRecyclerView))
             .check(matches(hasDescendant(withText(reminder.title))))
         onView(withId(R.id.reminderssRecyclerView))
@@ -80,18 +77,20 @@ class ReminderListFragmentTest {
             .check(matches(hasDescendant(withText(reminder.location))))
 
         onView(withId(R.id.noDataTextView))
-            .check(doesNotExist())
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
     }
 
     @Test
-    fun reminderListFragment_noData_checkNoData() = runTest {
+    fun reminderListFragment_noData_checkNoData() {
         // GIVEN: no data
 
         // WHEN: launch ReminderListFragment to display the list of reminders
         launchFragmentInContainer<ReminderListFragment>(themeResId = R.style.AppTheme)
 
-        // THEN: check that data is displayed
+        // THEN: check that the "no data" view is visible on screen
 
+        onView(withId(R.id.noDataTextView))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
         onView(withId(R.id.noDataTextView))
             .check(matches(withText(appContext.getString(R.string.no_data))))
 
@@ -100,12 +99,12 @@ class ReminderListFragmentTest {
     @Test
     fun reminderListFragment_navigateToSaveReminderFragment() {
         // GIVEN: launch ReminderListFragment to display the list of reminders
-        val scenario =
+        val fragmentScenario =
             launchFragmentInContainer<ReminderListFragment>(themeResId = R.style.AppTheme)
 
         // WHEN: instantiate the navigation controller and navigate to SaveReminderFragment
         val navController = mock(NavController::class.java)
-        scenario.onFragment {
+        fragmentScenario.onFragment {
             Navigation.setViewNavController(it.view!!, navController)
         }
         onView(withId(R.id.addReminderFAB))
