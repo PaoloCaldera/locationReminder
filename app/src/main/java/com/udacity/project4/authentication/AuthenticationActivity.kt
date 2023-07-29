1e4d966734b76127d75afae97ade1bf5a94b2e55
PaoloCaldera <paolo.caldera@outlook.com>
2023-07-29 11:56:55 +0200

    Change main activity and location permission control

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -77,11 +77,7 @@ class AuthenticationActivity : AppCompatActivity() {
      */
     private fun exitLogin() {
         val intent = Intent(this, RemindersActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
-    }
-
-    companion object {
-        private const val TAG = "AuthenticationActivity"
     }
 }
